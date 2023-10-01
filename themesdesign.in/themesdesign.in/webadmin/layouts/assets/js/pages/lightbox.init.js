970e76bbef62e7b4eacd3037f3aac2f237427bbf
viet tranvan <50480801+tranquocviet02@users.noreply.github.com>
2023-10-01 15:23:42 +0700

    okok
    okokok

--- themesdesign.in/themesdesign.in/webadmin/layouts/assets/js/pages/lightbox.init.js
+++ themesdesign.in/themesdesign.in/webadmin/layouts/assets/js/pages/lightbox.init.js
@@ -1,1 +1,11 @@
-var lightbox=GLightbox({selector:".image-popup",title:!1}),lightboxDesc=GLightbox({selector:".image-popup-desc"}),lightboxvideo=GLightbox({selector:".image-popup-video-map",title:!1});
+var lightbox = GLightbox({
+    selector: ".image-popup",
+    title: !1
+})
+  , lightboxDesc = GLightbox({
+    selector: ".image-popup-desc"
+})
+  , lightboxvideo = GLightbox({
+    selector: ".image-popup-video-map",
+    title: !1
+});
